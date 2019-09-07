9c8b48f55df196be94e6e97e0f857e2eb452c1d2
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-09-06 20:11:41 -0700

    Allow filtering nested SiteData by parameter_type in SiteType

--- app/graphql/types/site_type.rb
+++ app/graphql/types/site_type.rb
@@ -5,5 +5,13 @@ module Types
     field :lat, Float, null: true
     field :lon, Float, null: true
     field :site_data, [Types::SiteDatumType], null: true
+    field :filtered_site_data, [Types::SiteDatumType], null: true do
+      argument :parameter_type, String, required: false, default_value: "current_temp"
+    end
+
+    def filtered_site_data(parameter_type:)
+      object.site_data.current.where(parameter_type: parameter_type)
+    end
+
   end
 end
--- app/models/site_datum.rb
+++ app/models/site_datum.rb
@@ -1,3 +1,8 @@
 class SiteDatum < ApplicationRecord
   belongs_to :site
+
+  scope :current, -> do
+    where("timestamp >= ?", Time.zone.now.utc.change(min: 0))
+    .order(timestamp: :asc)
+  end
 end
