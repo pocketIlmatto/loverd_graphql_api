038a6a9f8b22989ecb8f45c1dbd1de73388e8db5
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-09-06 20:10:57 -0700

    Add id to SiteDatumType

--- app/graphql/types/site_datum_type.rb
+++ app/graphql/types/site_datum_type.rb
@@ -1,5 +1,6 @@
 module Types
   class SiteDatumType < Types::BaseObject
+    field :id, Integer, null: false
     field :parameter_type, String, null: false
     field :timestamp, Types::DateTimeType, null: false
     field :value, String, null: true
